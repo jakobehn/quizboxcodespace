fb546f60591bcfcd3a9904982b6951338d72c222
Jakob Ehn <jakob@ehn.nu>
2015-05-15 15:36:19 +0200

    Changed path of web depoy package

--- Source/QBox.Web/Deploy/PublishWebSite.ps1
+++ Source/QBox.Web/Deploy/PublishWebSite.ps1
@@ -1,1 +1,1 @@
-Publish-AzureWebsiteProject -Name QBox-dev -Package $applicationPath\_PublishedWebsites\QBox.Web_Package\QBox.Web.zip
+Publish-AzureWebsiteProject -Name QBox-dev -Package $applicationPath\QBox.Web.zip
