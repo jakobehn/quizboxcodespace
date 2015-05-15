7f9ed722dba232ec0548b430b780770509888553
Jakob Ehn <jakob@ehn.nu>
2015-05-15 16:59:47 +0200

    Check if site exists before creating it

--- Source/QBox.Web/Deploy/PublishWebSite.ps1
+++ Source/QBox.Web/Deploy/PublishWebSite.ps1
@@ -1,1 +1,9 @@
+if( Test-AzureName -Website QBox-dev)
+{
+	Write-Verbose 'Site already exists'
+}
+else
+{
+	New-AzureWebsite -Name QBox-dev -Location "North Europe"
+}
 Publish-AzureWebsiteProject -Name QBox-dev -Package $applicationPath\QBox.Web.zip
