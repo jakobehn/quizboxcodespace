3344a4e68ffbca6c41f2970631d52c238e8e191a
Jakob Ehn <jakob@ehn.nu>
2015-05-17 20:58:26 +0200

    Incorrect env variable names

--- Source/QBox.Web/Deploy/CopyDeploymentScripts.ps1
+++ Source/QBox.Web/Deploy/CopyDeploymentScripts.ps1
@@ -1,4 +1,4 @@
-$sourcePath = "$(TF_BUILD_SOURCESDIRECTORY)\Source\QBox.Web\Deploy\*.ps1"
+$sourcePath = "$Env:TF_BUILD_SOURCESDIRECTORY\Source\QBox.Web\Deploy\*.ps1"
 $destinationPath = "$Env:TF_BUILD_BINARIESDIRECTORY\Deploy"
 
 Write-Verbose -Verbose ("Copying ps1 files " + $sourcePath + " to " + $destinationPath)
