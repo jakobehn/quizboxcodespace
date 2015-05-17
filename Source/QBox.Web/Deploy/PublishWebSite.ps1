169fb0f12233a5831ea72dc84370e52909c0f2f0
Jakob Ehn <jakob@ehn.nu>
2015-05-17 18:24:59 +0200

    Adding comments to publishing script

--- Source/QBox.Web/Deploy/PublishWebSite.ps1
+++ Source/QBox.Web/Deploy/PublishWebSite.ps1
@@ -1,9 +1,15 @@
+Write-Verbose "Checking if site already exists"
 if( Test-AzureName -Website QBox-dev)
 {
 	Write-Verbose 'Site already exists'
 }
 else
 {
+	Write-Verbose 'Creating web site'
 	New-AzureWebsite -Name QBox-dev -Location "North Europe"
 }
+
+Write-Verbose 'Publishing web site'
 Publish-AzureWebsiteProject -Name QBox-dev -Package $applicationPath\QBox.Web.zip
+
+Write-Verbose 'Deployment done!'
