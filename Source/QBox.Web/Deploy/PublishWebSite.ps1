e2607eb619907fae45a91d8dc9dd1a60d3b06b47
Jakob Ehn <jakob@ehn.nu>
2015-05-18 10:13:14 +0200

    Refactored publish script

--- Source/QBox.Web/Deploy/PublishWebSite.ps1
+++ Source/QBox.Web/Deploy/PublishWebSite.ps1
@@ -1,31 +1,33 @@
+function CreateWebSite([string]$siteName)
+{
+	Write-Verbose -verbose "Checking if $siteName already exists"
+	if( Test-AzureName -Website "$siteName")
+	{
+		Write-Verbose -verbose "Site $siteName already exists"
+	}
+	else
+	{
+		Write-Verbose -verbose "Creating $siteName"
+		New-AzureWebsite -Name "$siteName" -Location "North Europe"
+	}
+}
+
+function PublishWebSite([string]$siteName, [string]$webDeployPackage)
+{
+	Write-Verbose -verbose "Publishing $siteName"
+	Publish-AzureWebsiteProject -Verbose -Name "$siteName" -Package "$webDeployPackage"
+	Write-Verbose -verbose "$siteName published"
+}
+
 $startTime = Get-Date
 Write-Verbose -Verbose "Starting deployment at $startTime"
 
-Write-Verbose -verbose "Checking if site already exists"
-if( Test-AzureName -Website QBox-dev)
-{
-	Write-Verbose -verbose 'Site already exists'
-}
-else
-{
-	Write-Verbose -verbose 'Creating web site'
-	New-AzureWebsite -Name QBox-dev -Location "North Europe"
-}
-
-Write-Verbose -verbose "Checking if api site already exists"
-if( Test-AzureName -Website QBoxApi-dev)
-{
-	Write-Verbose -verbose 'Api site already exists'
-}
-else
-{
-	Write-Verbose -verbose 'Creating api site'
-	New-AzureWebsite -Name QBoxApi-dev -Location "North Europe"
-}
+CreateWebSite "QBox-Dev"
+CreateWebSite "QBoxApi-Dev"
 
 Write-Verbose -Verbose 'Setting environment specific variables'
 
-$paramFiles=get-childitem . *.SetParameters.xml -rec
+$paramFiles=get-childitem "$applicationPath" "*.SetParameters.xml" -rec -Verbose
 foreach ($file in $paramFiles)
 {
 	Write-Verbose -Verbose ("Updating token __APIURL__ in file " + $file.PSPath + " with $APIURL")
@@ -33,8 +35,9 @@ foreach ($file in $paramFiles)
 	Foreach-Object {$_ -replace "__APIURL__", "$APIURL"} | 
 	Set-Content $file.PSPath
 }
-Write-Verbose -verbose 'Publishing api site'
-Publish-AzureWebsiteProject -Verbose -Name QBoxApi-dev -Package $applicationPath\QBox.Api.zip
+
+PublishWebSite "QBox-Dev" $applicationPath\QBox.Web.zip
+PublishWebSite "QBoxApi-Dev" $applicationPath\QBox.Api.zip
 
 $endTime = Get-Date
 Write-Verbose -Verbose "Finished deployment at $endTime"
