ab9440be79dc27983f20156c4b138fb51768d2aa
Jakob Ehn <jakob@ehn.nu>
2015-05-21 13:13:30 +0200

    updated variable names

--- Source/QBox.Web/Deploy/DeployDatabase.ps1
+++ Source/QBox.Web/Deploy/DeployDatabase.ps1
@@ -1,6 +1,6 @@
 ﻿$path = ${env:ProgramFiles(x86)} + "\Microsoft SQL Server\120\DAC\bin\SqlPackage.exe”
-$dacpacFullPath = join-path $applicationPath $dacpac
+$dacpacFullPath = join-path $applicationPath $DBdacpac
 
-$output = & $path /Action:Publish /SourceFile:"$dacpacFullPath" /TargetDatabaseName:$Database /TargetServerName:$TargetServer /TargetUser:$UserName /TargetPassword:$Password 2>&1 
+$output = & $path /Action:Publish /SourceFile:"$dacpacFullPath" /TargetDatabaseName:$DBDatabase /TargetServerName:$DBTargetServer /TargetUser:$DBUserName /TargetPassword:$DBPassword 2>&1 
 
 Write-Verbose ($output | Out-String) -Verbose
