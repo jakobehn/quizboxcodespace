e8088a1a64c46ff2dedbb19579a862372eec1470
Jakob Ehn <jakob@ehn.nu>
2015-05-21 10:59:52 +0200

    minor changes

--- Source/QBox.Web/Controllers/CategoryController.cs
+++ Source/QBox.Web/Controllers/CategoryController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using QBox.Api.Client;
 using QBox.Web.Models;
@@ -19,20 +17,13 @@ namespace QBox.Web.Controllers
 
         public ActionResult Index()
         {
-            try
+            var allCategories = apiClient.GetCategories().Result;
+            var model = new QuizCategoriesViewModel
             {
-                var allCategories = apiClient.GetCategories().Result;
-                var model = new QuizCategoriesViewModel
-                {
-                    Categories = allCategories.Select(
-                        c => new QuizCategoryViewModel() {Id = c.Id, Name = c.Name, Description = c.Description}).ToList()
-                };
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                return View();
-            }
+                Categories = allCategories.Select(
+                    c => new QuizCategoryViewModel() {Id = c.Id, Name = c.Name, Description = c.Description}).ToList()
+            };
+            return View(model);
         }
 
         [HttpPost]
@@ -44,8 +35,6 @@ namespace QBox.Web.Controllers
             }
 
             string selectedCategory = Convert.ToString(form[0]);
-
-            //Create game
             return RedirectToAction("Index", "Question", new {category=selectedCategory});
         }
 
