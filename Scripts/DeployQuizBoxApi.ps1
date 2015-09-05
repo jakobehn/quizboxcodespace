bea6cc4f4811b58bad0c17b1cf78357e035ab4f3
Jakob Ehn <jakob@ehn.nu>
2015-09-05 20:33:40 +0200

    set working directory

--- Scripts/DeployQuizBoxApi.ps1
+++ Scripts/DeployQuizBoxApi.ps1
@@ -1,1 +1,14 @@
-$(Agent.ReleaseDirectory)\drop\qbox.api.deploy.cmd /Y
+[CmdletBinding()]
+
+param(
+	[Parameter(Position=1)]
+	[string]$workingDirectory
+)
+
+if( $workingDirectoru)
+{
+	Write-Verbose -Verbose "Setting working directory to " + $workingDirectory
+	Set-Location $workingDirectory
+}
+
+qbox.api.deploy.cmd /Y
