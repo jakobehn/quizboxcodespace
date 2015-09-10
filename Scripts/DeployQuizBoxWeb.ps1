9f592eed4906d448b8f62681a8beeba8455e55e4
Jakob Ehn <jakob@ehn.nu>
2015-09-10 14:14:38 +0200

    no need to add path since we executing this on the build agent, so the PS task supports wokring directory

--- Scripts/DeployQuizBoxWeb.ps1
+++ Scripts/DeployQuizBoxWeb.ps1
@@ -1,15 +1,2 @@
-[CmdletBinding()]
-
-param(
-	[Parameter(Position=1)]
-	[string]$workingDirectory
-)
-
-if( $workingDirectory)
-{
-	Write-Verbose -Verbose ("Setting working directory to " + $workingDirectory)
-	Set-Location $workingDirectory
-}
-
 .\QBox.Web.deploy.cmd /Y
 
