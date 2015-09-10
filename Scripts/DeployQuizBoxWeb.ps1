dcfd5e91e23da072cd4ba2ef86de4f5290d1fec6
Jakob Ehn <jakob@ehn.nu>
2015-09-10 14:57:01 +0200

    Adding parameters to deploy script

--- Scripts/DeployQuizBoxWeb.ps1
+++ Scripts/DeployQuizBoxWeb.ps1
@@ -1,2 +1,13 @@
-.\QBox.Web.deploy.cmd /Y
+[CmdletBinding()]
+
+param(
+	[Parameter(Position=1)]
+	[string]$publishUrl,
+		[Parameter(Position=2)]
+	[string]$userName,
+		[Parameter(Position=3)]
+	[string]$password
+)
+
+.\QBox.Web.deploy.cmd /Y "/M:$publishUrl" /u:$userName /p:$password /a:Basic
 
