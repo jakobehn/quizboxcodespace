5497d267e8bac57a71a4bb557a8c347148671180
Jakob Ehn <jakob@ehn.nu>
2015-09-29 09:06:10 +0200

    moved init script into main script

--- Scripts/ConfigureWebServer.ps1
+++ Scripts/ConfigureWebServer.ps1
@@ -42,5 +42,9 @@ Configuration QuizBoxApiWebSite
 		}
    }
 }
+#Copy DSC modules into system modules folder
+$customModulesDirectory = Join-Path $env:SystemDrive "\Program Files\WindowsPowerShell\Modules"
+$customModuleSrc = Join-Path $applicationPath "xWebAdministration"
 
+Copy-Item -Verbose -Force -Recurse -Path $customModuleSrc -Destination $customModulesDirectory
 QuizBoxApiWebSite -Verbose
