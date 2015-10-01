2c81f3350d351e54259d770903f3c4d605244b26
Jakob Ehn <jakob@ehn.nu>
2015-10-01 10:39:47 +0200

    changed back again

--- Scripts/ConfigureWebServer.ps1
+++ Scripts/ConfigureWebServer.ps1
@@ -1,9 +1,3 @@
-# Copy DSC modules into system modules folder
-$customModulesDirectory = Join-Path $env:SystemDrive "\Program Files\WindowsPowerShell\Modules"
-$customModuleSrc = Join-Path $applicationPath "xWebAdministration"
-
-Copy-Item -Verbose -Force -Recurse -Path $customModuleSrc -Destination $customModulesDirectory
-
 Configuration QuizBoxApiWebSite
 {
 	Import-DSCResource -ModuleName xWebAdministration
