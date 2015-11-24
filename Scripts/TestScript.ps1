669e0f5dc360c9414c42f51bc828861623caae09
Jakob Ehn <jakob@ehn.nu>
2015-11-24 20:15:56 +0000

    Deleted /Scripts/TestScript.ps1

--- Scripts/TestScript.ps1
+++
@@ -1,6 +0,0 @@
-$VerbosePreference = "Continue"
-Write-Output "Write-Output"
-Write-Verbose "Write-Verbose"
-
-Write-Output -Verbose "Write-Output -Verbose"
-Write-Verbose -Verbose "Write-Verbose -Verbose"
