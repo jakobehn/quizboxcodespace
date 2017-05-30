cafdbae4cfbea379fe5ae168003546e6bda13271
Jakob Ehn <jakob@ehn.nu>
2017-05-30 23:38:10 +0200

    added some tets

--- Source/QBox.Api.UnitTests/HighscoreTests.cs
+++ Source/QBox.Api.UnitTests/HighscoreTests.cs
@@ -6,11 +6,11 @@ using System.Threading;
 namespace QBox.Api.UnitTests
 {
     [TestFixture]
-    //[Parallelizable]
 
     public class HighscoreTests
     {
         [Test]
+        [Category("Integration")]
         public void PostTopHighscoreShouldStoreScoreAtTop()
         {
             Thread.Sleep(2000);
@@ -18,11 +18,53 @@ namespace QBox.Api.UnitTests
 
 
         [Test]
+        [Category("Integration")]
         public void CategoryMustMatchIndex()
         {
             Thread.Sleep(2000);
         }
 
+        [Test]
+        [Category("Integration")]
+        public void StartNewGame()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void FinishGame()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void SelectRandomCategory()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void SelectRandomCategoryTwice()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void SelectFirstCategory()
+        {
+            Thread.Sleep(2000);
+        }
+        [Test]
+        [Category("Integration")]
+        public void SelectLastCategory()
+        {
+            Thread.Sleep(2000);
+        }
+
     }
-   
+
 }
--- Source/QBox.Api.UnitTests/PlayerTests.cs
+++ Source/QBox.Api.UnitTests/PlayerTests.cs
@@ -4,20 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using Xunit;
 
 namespace QBox.Api.UnitTests
 {
+    [TestFixture]
     public class PlayerTests
     {
-        [Fact(DisplayName = "PlayerNameShouldBeValid")]
-        public void  PlayerNameShouldBeValid()
+        [Test]
+        [Category("Integration")]
+        public void PlayerNameShouldBeValid()
         {
             Thread.Sleep(2000);
         }
 
-        [Fact(DisplayName = "PlayerAgeMustBeValid")]
-        public void PlayerAgeMustBeValid()
+        [Test]
+        [Category("Integration")]
+        public void PlayerAgeCanBeChanged()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void PlayerNameMustBeUnique()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void PlayerAgeCantBeLowerThanZero()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void PlayerNameMustContainLettersOnly()
+        {
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void PlayerNameMustBeLocal()
         {
             Thread.Sleep(2000);
         }
