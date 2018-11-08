46b5329b7b04e9f93fec071bcb7b505508bc45b2
Jakob Ehn <jakob@ehn.nu>
2018-11-08 13:55:46 +0100

    winops conf

---
+++ Source/QBox.ClientJS/calculator.js
@@ -0,0 +1,17 @@
+var calculator = (function () {
+    function calculator() {
+    }
+    calculator.prototype.add = function (num1, num2) {
+        return num1 + num2;
+    };
+    calculator.prototype.sub = function (num1, num2) {
+        return num1 - num2;
+    };
+    calculator.prototype.multiply = function (num1, num2) {
+        return num1 * num2;
+    };
+    calculator.prototype.divide = function (num1, num2) {
+        return num1 / num2;
+    };
+    return calculator;
+}());
