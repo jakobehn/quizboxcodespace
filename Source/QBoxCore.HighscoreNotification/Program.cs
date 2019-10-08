852c62f2da4f17607e1d7fecd63b9e487ba46016
Jakob Ehn <jakob@ehn.nu>
2019-10-08 14:48:45 +0200

    added console app

---
+++ Source/QBoxCore.HighscoreNotification/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace QBoxCore.HighscoreNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Waiting for highscore notifications");
+
+            while(true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
