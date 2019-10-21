b569fed1e165220852190f89d52c510ce1d18640
Jakob Ehn <jakob@ehn.nu>
2019-10-21 07:56:50 +0200

    asdas ***NO_CI***

--- k8s/enable-virtual-nodes.ps1
+++ k8s/enable-virtual-nodes.ps1
@@ -1,3 +1,15 @@
+[CmdletBinding()]
+param (
+    [string]$resourceGroup,
+    [string]$vnet,
+    [string]$appId
+)
+
+$vnetId = (az network vnet show --resource-group $resourceGroup --name $vnet --query id -o tsv)
+
+
+az role assignment create --assignee $appId --scope $vnetId --role Contributor
+
 az aks enable-addons `
     --resource-group tdswe-test `
     --name tdswe-test `
